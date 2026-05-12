71c54757498b07f223001cef5108a762952aaee5
Ívna Mascarenhas e Abreu <ivna.mabreu@gmail.com>
2026-05-12 21:17:19 +0000

    Add aulas 006, 007 e 008

---
+++ aulas/006/app.py
@@ -0,0 +1,8 @@
+string = 'Python é legal'
+print(string[5])
+print(string[-3])
+print(string[0:6])
+print(string[5:])
+print(string[:-5])
+string2 = string[:]
+print (string2)
---
+++ aulas/007/app.py
@@ -0,0 +1,6 @@
+nome = "Ana"
+sobrenome = "Pereira"
+mensagem = nome + " " + sobrenome + ' é aluna nova.'
+print(mensagem)
+mensagem = f'{nome} {sobrenome} é aluna nova.'
+print(mensagem)
