c8090f8233d94eb2e57bbe881672ba3fc4771a9f
Ívna Mascarenhas e Abreu <ivna.mabreu@gmail.com>
2026-05-20 15:11:54 +0000

    Add aulas 009 e 010

---
+++ aulas/009/app.py
@@ -0,0 +1,17 @@
+x = 10 + 3
+a = 10 - 3
+b = 10 * 3
+c = 10 / 3
+d = 10 // 3
+e = 10 % 3
+f = 10 ** 3
+print(x)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+y = 10
+y += 3
+print(y)
---
+++ aulas/010/app.py
@@ -0,0 +1,13 @@
+x = 10 + 3 * 2
+y = (10 + 3) * 2
+a = 10 + 3 * 2 ** 3
+b = 10 + (3 * 2) ** 3
+print(x)
+print(y)
+print(a)
+print(b)
+
+# A ordem das operações em Python segue a regra matemática:
+# Exponenciação: **
+# Multiplicação e divisão: *, /, //, %
+# Adição e subtração: +, -
