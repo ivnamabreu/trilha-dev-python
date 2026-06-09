f8a01e93666e955dc1776fd728de229aa1439f67
Ívna Mascarenhas e Abreu <ivna.mabreu@gmail.com>
2026-06-09 20:54:26 +0000

    Add aula 12

---
+++ aulas/012/app.py
@@ -0,0 +1,12 @@
+esta_quente = False
+esta_frio = False
+
+if esta_quente:
+    print('O dia está quente.')
+    print('Beba água!')
+elif esta_frio:
+    print('O dia está frio.')
+    print('Use agasalho!')
+else:
+    print('É um lindo dia!')
+print('Tenha um bom dia!')
